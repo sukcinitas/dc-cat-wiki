c8995b1ed5e368fb5e5503cad887b3583791d4c1
Gintarė Černiauskaitė <gintare.cerniauskaite@protonmail.com>
2021-05-08 18:27:19 +0300

    Update API URL

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 import App from './App';
 
-const location = window.location.href === 'http://localhost:4000/#/' ? 'http://localhost:4000' : 'https://sheltered-lowlands-67991.herokuapp.com//';
+const location = window.location.href === 'http://localhost:4000/#/' ? 'http://localhost:4000' : 'https://sheltered-lowlands-67991.herokuapp.com/';
 axios.defaults.baseURL = location;
 
 ReactDOM.render(
