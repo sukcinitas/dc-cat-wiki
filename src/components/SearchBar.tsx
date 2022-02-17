a814717864ef908373d2467ddbc7384df11cb99a
Gintare Cerniauskaite <gintare.cerniauskaite@protonmail.com>
2022-02-17 19:52:31 +0200

    Change behavior: list breeds first time search bar is in focus

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useHistory } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 import '../sass/SearchBar.scss';
@@ -11,22 +11,19 @@ const SearchBar = () => {
   const [error, setError] = useState('');
   const [isModalVisible, setIsModalVisible] = useState(false);
   const history = useHistory();
-  const firstUpdate = useRef(true);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setSearchQuery(e.currentTarget.value);
     setError('');
-  }
+  };
 
   useEffect(() => {
-    if (firstUpdate.current) {
-      firstUpdate.current = false;
-      return;
-    }
     const searchBreed = async (searchQuery: string): Promise<void> => {
       axios.get(`/api/cats/search?q=${searchQuery}`).then(
         (res) => {
-          const { data: { success, searchList, message }} = res;
+          const {
+            data: { success, searchList, message },
+          } = res;
           if (success) {
             if (searchList.length === 1 && searchList[0].name.toLowerCase() === searchQuery.toLowerCase()) {
               setSelectedBreedId(searchList[0].id);
@@ -39,24 +36,22 @@ const SearchBar = () => {
           }
         },
         (err) => {
-          setError(
-            err.response.data.message ||
-              `${err.response.status}: ${err.response.statusText}`,
-          );
+          setError(err.response.data.message || `${err.response.status}: ${err.response.statusText}`);
           setSearchQuery('');
-        });
+        }
+      );
     };
     searchBreed(searchQuery);
   }, [searchQuery]);
 
-  const selectBreed = (breedName:string, breedId:string, e?: React.KeyboardEvent): void => {
+  const selectBreed = (breedName: string, breedId: string, e?: React.KeyboardEvent): void => {
     if (e && e.key !== '13') {
       return;
     }
     setSearchQuery(breedName);
     setSelectedBreedId(breedId);
     history.push(`/breeds/${breedId}`);
-  }
+  };
 
   const handleSubmit = (e: React.FormEvent): void => {
     e.preventDefault();
@@ -64,56 +59,69 @@ const SearchBar = () => {
       return;
     }
     history.push(`/breeds/${selectedBreedId}`);
-  }
+  };
 
   const closeModal = () => {
     setIsModalVisible(false);
     setSearchQuery('');
     setList([]);
-  }
+  };
 
-  const nameList = list.map((cat: { name: string, id: string }): React.ReactElement => (
-    <li tabIndex={0} onClick={() => selectBreed(cat.name, cat.id)} onKeyDown={(e) => selectBreed(cat.name, cat.id, e)} key={cat.id} className="search__item">{cat.name}</li>
-  ));
+  const nameList = list.map(
+    (cat: { name: string; id: string }): React.ReactElement => (
+      <li
+        tabIndex={0}
+        onClick={() => selectBreed(cat.name, cat.id)}
+        onKeyDown={(e) => selectBreed(cat.name, cat.id, e)}
+        key={cat.id}
+        className="search__item"
+      >
+        {cat.name}
+      </li>
+    )
+  );
 
   return (
     <div className="search">
       <form className="search__bar" onSubmit={handleSubmit}>
-        <input 
+        <input
           placeholder={error ? error : 'Enter your breed'}
           className="search__bar-input"
-          onChange={handleChange} value={searchQuery}
+          onChange={handleChange}
+          value={searchQuery}
           type="text"
           onClick={() => setIsModalVisible(true)}
         />
-        <button type="submit" className="search__icon"><span className="material-icons">search</span></button>
+        <button type="submit" className="search__icon">
+          <span className="material-icons">search</span>
+        </button>
       </form>
-      <button className="search__button" onClick={() => setIsModalVisible(true)}>Search
+      <button className="search__button" onClick={() => setIsModalVisible(true)}>
+        Search
         <span className="material-icons search__button-icon">search</span>
       </button>
-      <div className="search__panel">
-        {list.length > 0 && <ul className="search__list">
-          {nameList}
-        </ul>}
-      </div>
+      <div className="search__panel">{list.length > 0 && <ul className="search__list">{nameList}</ul>}</div>
       <div className={isModalVisible ? 'modal' : 'modal--hidden'}>
-        <button onClick={closeModal} className="modal__btn"><span className="material-icons search__icon--close modal__icon--close">close</span></button>
+        <button onClick={closeModal} className="modal__btn">
+          <span className="material-icons search__icon--close modal__icon--close">close</span>
+        </button>
         <form className="modal__bar search__bar" onSubmit={handleSubmit}>
-          <input 
+          <input
             placeholder={error ? error : 'Enter your breed'}
             className="modal__bar-input search__bar-input"
-            onChange={handleChange} value={searchQuery}
+            onChange={handleChange}
+            value={searchQuery}
             onFocus={handleChange}
             type="text"
           />
-          <button type="submit" className="modal__icon search__icon"><span className="material-icons">search</span></button>
+          <button type="submit" className="modal__icon search__icon">
+            <span className="material-icons">search</span>
+          </button>
         </form>
-      <div className="modal__panel search__panel">
-        {list.length > 0 && <ul className="modal__list search__list">
-          {nameList}
-        </ul>}
+        <div className="modal__panel search__panel">
+          {list.length > 0 && <ul className="modal__list search__list">{nameList}</ul>}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
