1b5f65c1d9eee18f4f218b53e75e62371e454336
Gintare Cerniauskaite <gintare.cerniauskaite@protonmail.com>
2022-09-08 20:18:22 +0300

    Update api adress

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import axios from "axios";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-import App from "./App";
+import App from './App';
 
 const location =
-  window.location.href === "http://localhost:4000/#/"
-    ? "http://localhost:4000"
-    : "https://sheltered-lowlands-67991.herokuapp.com/";
+  window.location.href === 'http://localhost:4000/#/'
+    ? 'http://localhost:4000'
+    : 'https://elemental-oil-tin.glitch.me/';
 
 axios.defaults.baseURL = location;
 
@@ -15,5 +15,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
